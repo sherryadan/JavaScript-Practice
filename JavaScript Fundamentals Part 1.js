596da17fad7d3e448cbbef0d4402b5f8f04c3c23
sherryadan <adanmohammad80@gmail.com>
2023-08-07 07:15:14 -0700

    New Commit

--- JavaScript Fundamentals Part 1.js
+++ JavaScript Fundamentals Part 1.js
@@ -1,4 +1,5 @@
 // These are the Coding Challenges of UDEMY COURSE of The Complete Javascript Code from Zero to Expert
+// New Commit
 
 
 // Javascript Fundamentals - Part 1
@@ -38,3 +39,23 @@ function highBMI(){
 }
 
 highBMI();
+
+// Challenge no 3
+sD1 = 91; sD2=88; sD3=110;
+sK1=88;sK2=91; sK3=110;
+
+const averageScoreDolphins = (sD1+sD2+sD3)/2;
+const averageScoreKoalas= (sK1+sK2+sK3)/2;
+
+function highaverage(){
+    if(averageScoreDolphins > averageScoreKoalas){
+        console.log("Dolphins Win")
+    }
+    else if(averageScoreKoalas>averageScoreDolphins){
+        console.log("Koalas Win")
+    }
+    else{
+        console.log("It's a Draw")
+    }
+}
+highaverage()
